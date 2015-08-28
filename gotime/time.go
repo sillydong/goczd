03f7d4906c56a400b6183ff31dfc5ab9832a910b
Chen.Zhidong <njutczd@gmail.com>
2015-08-28 16:59:23 +0800

    添加time字段

--- gotime/time.go
+++ gotime/time.go
@@ -4,10 +4,20 @@ import "time"
 const(
 	FORMAT_YYYY_MM_DD="2006-01-02"
 	FORMAT_YYYY_MM_DD_HH_II_SS="2006-01-02 15:04:05"
+	Y="2016"
+	M="01"
+	D="02"
+	H="15"
+	I="04"
+	S="05"
 )
 
 func GetTimestamp()(int64){
 	return time.Now().Unix()
+}
+
+func GetTimeStr(format string)(string){
+	return time.Now().Format(format)
 }
 
 func StrToTime(str,format string)(int64,error){
