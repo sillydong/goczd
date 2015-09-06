54c3565906d44f7e4624acd8a7e87a286acf5005
Chen.Zhidong <njutczd@gmail.com>
2015-09-06 23:40:35 +0800

    封装echo的分页函数及模块接口

---
+++ goecho/module.go
@@ -0,0 +1,12 @@
+package goecho
+
+type Module interface {
+	InitRoute()
+}
+
+type StructResponse struct {
+	Errno int         `json:errno,omitempty`
+	Error string      `json:"error,omitempty"`
+	Total int64         `json:total,omitempty`
+	Data  interface{} `json:data,omitempty`
+}
---
+++ goecho/pagination.go
@@ -0,0 +1,43 @@
+package goecho
+
+import (
+	"math"
+)
+
+type Pagination struct {
+	ItemCount   uint
+	CurrentPage uint
+
+	PageSize  uint
+	PageCount uint
+}
+
+func NewPagination(currentpage, itemcount uint) *Pagination {
+	return &Pagination{
+		CurrentPage: currentpage,
+		ItemCount:   itemcount,
+	}
+}
+
+func (pagination *Pagination) Calc(pagesize uint) *Pagination {
+	pagination.PageSize = pagesize
+	pagination.PageCount = pagination.getPageCount()
+	if pagination.CurrentPage >= pagination.PageCount {
+		pagination.CurrentPage = pagination.PageCount - 1
+	}
+	return pagination
+}
+
+func (pagination *Pagination) getPageCount() uint {
+	return uint(math.Ceil(float64(pagination.ItemCount + pagination.PageSize - 1) / float64(pagination.PageSize)))
+}
+
+func (pagination *Pagination) getOffset() uint {
+	return pagination.CurrentPage * pagination.PageSize
+}
+
+func (pagination *Pagination) ApplyLimit() (limit, offset uint) {
+	limit = pagination.PageSize
+	offset = pagination.getOffset()
+	return
+}
