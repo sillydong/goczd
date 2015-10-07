3220f4314ce97e7cf3dba3e13a31c6a9cc598429
Chen.Zhidong <njutczd@gmail.com>
2015-10-07 21:49:58 +0800

    增加友好的时间显示，增加注释

--- gotime/time.go
+++ gotime/time.go
@@ -1,6 +1,9 @@
 package gotime
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const (
 	FORMAT_YYYY_MM_DD          = "2006-01-02"
@@ -13,14 +16,17 @@ const (
 	S                          = "05"
 )
 
+//获取当前时间戳
 func GetTimestamp() int64 {
 	return time.Now().Unix()
 }
 
+//按照指定格式显示当前时间
 func GetTimeStr(format string) string {
 	return time.Now().Format(format)
 }
 
+//字符串转时间戳
 func StrToTime(str, format string) (int64, error) {
 	timetime, err := time.Parse(format, str)
 	if err != nil {
@@ -30,6 +36,28 @@ func StrToTime(str, format string) (int64, error) {
 	}
 }
 
+//时间戳转字符串
 func TimeToStr(timestamp int64, format string) string {
 	return time.Unix(timestamp, 0).Format(format)
+}
+
+//友好的时间显示
+func FriendlyTime(timestamp int64) string {
+	current := time.Now()
+	diff := current.Unix() - timestamp
+
+	switch {
+	case diff > 0 && diff < 60:
+		return fmt.Sprintf("%d秒前", diff)
+	case diff >= 60 && diff < 3600:
+		return fmt.Sprintf("%d分钟前", diff/60)
+	case diff >= 3600 && diff < 86400:
+		return fmt.Sprintf("%d小时前", diff/3600)
+	case diff >= 86400 && diff < 86400*2:
+		return "昨天"
+	case diff >= 86400*2 && diff < 86400*3:
+		return "前天"
+	default:
+		return TimeToStr(timestamp, Y+"-"+M+"-"+D)
+	}
 }
