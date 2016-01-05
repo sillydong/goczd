52af0b27b54483efb90e8afe77dcd5e4b5477a7b
Chen.Zhidong <njutczd@gmail.com>
2016-01-05 21:22:43 +0800

    完善beego缓存初始化代码

--- gobee/init.go
+++ gobee/init.go
@@ -1,21 +1,24 @@
 package gobee
+
 import (
 	"git.sillydong.com/chenzhidong/goczd/gofile"
-	"path/filepath"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/cache"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
+	"path/filepath"
 	//_ "github.com/mattn/go-sqlite3"
-	_ "github.com/astaxie/beego/cache/memcache"
 	"fmt"
+	_ "github.com/astaxie/beego/cache/memcache"
 	"github.com/garyburd/redigo/redis"
+	"os"
 	"runtime"
+	"syscall"
 	"time"
 )
 
-func InitLog(){
+func InitLog() {
 	beego.Info("initiating log")
 	logconf, err := gofile.IoUtilReadFile(filepath.Join(beego.AppPath, "conf", "log.json"))
 	if err != nil {
@@ -25,59 +28,137 @@ func InitLog(){
 	}
 }
 
-func InitMemcache() {
+//初始化memcache缓存，配置参数如下
+//
+// memcache_host memcache主机(127.0.0.1)
+//
+// memcache_port memcache端口(11211)  
+func InitMemcache() (cache.Cache, error) {
 	beego.Info("initiating memcache")
-	memconf := beego.AppConfig.DefaultString("memcache", "")
-	if len(memconf) >= 0 {
-		_, err := cache.NewCache("memcache", memconf)
-		if err != nil {
-			panic(err)
-		}
-	}else{
-		panic("missing memcache config")
-	}
+	memcache_host := beego.AppConfig.DefaultString("memcache_host", "127.0.0.1")
+	memcache_port := beego.AppConfig.DefaultString("memcache_port", "11211")
+	return cache.NewCache("memcache", fmt.Sprintf(`{"conn":"%s:%s"`, memcache_host, memcache_port))
 }
 
+//初始化文件缓存，配置参数如下
+//
+// filecache_dir 缓存文件目录
+//
+// filecache_suffix 缓存文件后缀(.cache)
+//
+// filecache_level 目录层级(2)
+//
+// filecache_expire 过期时间(3600秒)
+func InitFilecache() (cache.Cache, error) {
+	beego.Info("initiating file cache")
+	filecache_dir := beego.AppConfig.DefaultString("filecache_dir", "")
+	filecache_suffix := beego.AppConfig.DefaultString("filecache_suffix", ".cache")
+	filecache_level := beego.AppConfig.DefaultInt("filecache_level", 2)
+	filecache_expire := beego.AppConfig.DefaultInt("filecache_expire", 3600)
+	if filecache_dir == "" {
+		panic("filecache_dir is not set")
+	}
+	info, err := os.Stat(filecache_dir)
+	if err != nil && !os.IsExist(err) {
+		if err := os.MkdirAll(filecache_dir, 777); err != nil {
+			panic(fmt.Sprintf("%s not exist and can not be created\n%v", filecache_dir, err))
+		}
+	}
+	if !info.IsDir() {
+		panic(fmt.Sprintf("%s is not a directory", filecache_dir))
+	}
+	if err := syscall.Access(filecache_dir, syscall.O_RDWR); err != nil {
+		panic(fmt.Sprintf("%s is not accessable\n%v", filecache_dir, err))
+	}
+	return cache.NewCache("file", fmt.Sprintf(`{"CachePath":"%s","FileSuffix":"%s","DirectoryLevel":%d,"EmbedExpiry":%d}`, filecache_suffix, filecache_suffix, filecache_level, filecache_expire))
+}
+
+//初始化内存缓存，配置参数如下
+//
+// memory_gc_interval 内存回收周期(60秒)
+func InitMemorycache() (cache.Cache, error) {
+	beego.Info("initiating memory cache")
+	memory_interval := beego.AppConfig.DefaultInt("memory_gc_interval", 60)
+	return cache.NewCache("memory", fmt.Sprintf(`{"interval":%d}`, memory_interval))
+}
+
+//初始化redis缓存，配置参数如下
+//
+// rediscache_host redis主机(127.0.0.1)
+//
+// rediscache_port redis端口(6379)
+func InitRediscache() (cache.Cache, error) {
+	beego.Info("initiating redis cache")
+	rediscache_host := beego.AppConfig.DefaultString("rediscache_host", "127.0.0.1")
+	rediscache_port := beego.AppConfig.DefaultString("rediscache_port", "6379")
+	return cache.NewCache("redis", fmt.Sprintf(`{"conn":"%s:%s"}`, rediscache_host, rediscache_port))
+}
+
+//初始化mysql或postgresql数据库，配置参数如下
+//
+// db_type 数据库类型，mysql或postgres(mysql)
+//
+// db_host 数据库主机地址(127.0.0.1)
+//
+// db_port 数据库端口(3306)
+//
+// db_user 数据库用户(root)
+//
+// db_pass 数据库密码
+//
+// db_name 数据库名
+//
+// db_sslmode 是否SSL模式连接，仅用于postgresql(disable)
 func InitDB() {
 	beego.Info("initiating db")
-	var dns string
-	db_type := beego.AppConfig.String("db_type")
-	db_host := beego.AppConfig.String("db_host")
-	db_port := beego.AppConfig.String("db_port")
-	db_user := beego.AppConfig.String("db_user")
-	db_pass := beego.AppConfig.String("db_pass")
+	var dsn string
+	db_type := beego.AppConfig.DefaultString("db_type", "mysql")
+	db_host := beego.AppConfig.DefaultString("db_host", "127.0.0.1")
+	db_port := beego.AppConfig.DefaultString("db_port", 3306)
+	db_user := beego.AppConfig.DefaultString("db_user", "root")
+	db_pass := beego.AppConfig.DefaultString("db_pass", "")
 	db_name := beego.AppConfig.String("db_name")
 	//db_path := beego.AppConfig.String("db_path")
-	db_sslmode := beego.AppConfig.String("db_sslmode")
+	db_sslmode := beego.AppConfig.DefaultString("db_sslmode", "disable")
 	switch db_type {
 	case "mysql":
 		orm.RegisterDriver("mysql", orm.DR_MySQL)
-		dns = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", db_user, db_pass, db_host, db_port, db_name)
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", db_user, db_pass, db_host, db_port, db_name)
 		break
 	case "postgres":
 		orm.RegisterDriver("postgres", orm.DR_Postgres)
-		dns = fmt.Sprintf("dbname=%s host=%s  user=%s  password=%s  port=%s  sslmode=%s", db_name, db_host, db_user, db_pass, db_port, db_sslmode)
-//	case "sqlite3":
-//		orm.RegisterDriver("sqlite3", orm.DR_Sqlite)
-//		if db_path == "" {
-//			db_path = "./"
-//		}
-//		dns = fmt.Sprintf("%s%s.db", db_path, db_name)
-//		break
+		dsn = fmt.Sprintf("dbname=%s host=%s  user=%s  password=%s  port=%s  sslmode=%s", db_name, db_host, db_user, db_pass, db_port, db_sslmode)
+		//	case "sqlite3":
+		//		orm.RegisterDriver("sqlite3", orm.DR_Sqlite)
+		//		if db_path == "" {
+		//			db_path = "./"
+		//		}
+		//		dns = fmt.Sprintf("%s%s.db", db_path, db_name)
+		//		break
 	default:
-		panic("Database driver is not allowed:"+db_type)
+		panic("Database driver is not allowed:" + db_type)
 	}
-	orm.RegisterDataBase("default", db_type, dns)
+	orm.RegisterDataBase("default", db_type, dsn)
 }
 
+//初始化redis数据库，配置参数如下
+//
+// redis_host redis主机(127.0.0.1)
+//
+// redis_port redis端口(6379)
+//
+// redis_pass redis密码
+//
+// redis_db redis使用的数据库(0)
 func InitRedis() *redis.Pool {
-	beego.Info("initiating redis")
-	redishost := beego.AppConfig.DefaultString("redis_host", "127.0.0.1:6379")
-	redispass := beego.AppConfig.DefaultString("redis_pass", "")
-	redisdb := beego.AppConfig.DefaultString("redis_db", "")
+	beego.Info("initiating redis db")
+	redis_host := beego.AppConfig.DefaultString("redis_host", "127.0.0.1")
+	redis_port := beego.AppConfig.DefaultString("redis_port", "6379")
+	redis_pass := beego.AppConfig.DefaultString("redis_pass", "")
+	redis_db := beego.AppConfig.DefaultString("redis_db", "")
 
 	return &redis.Pool{
-		MaxIdle: runtime.NumCPU(),
+		MaxIdle:     runtime.NumCPU(),
 		IdleTimeout: 180 * time.Second,
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
 			_, err := c.Do("PING")
@@ -87,17 +168,17 @@ func InitRedis() *redis.Pool {
 			return err
 		},
 		Dial: func() (redis.Conn, error) {
-			conn, err := redis.DialTimeout("tcp", redishost, 1*time.Second, 1*time.Second, 1*time.Second)
+			conn, err := redis.DialTimeout("tcp", fmt.Sprintf("%s:%s", redis_host, redis_port), 1*time.Second, 1*time.Second, 1*time.Second)
 			if err != nil {
 				return nil, err
 			}
-			if redispass != "" {
-				if _, err := conn.Do("AUTH", redispass); err != nil {
+			if redis_pass != "" {
+				if _, err := conn.Do("AUTH", redis_pass); err != nil {
 					return nil, err
 				}
 			}
-			if redisdb != "" {
-				if _, err := conn.Do("SELECT", redisdb); err != nil {
+			if redis_db != "" {
+				if _, err := conn.Do("SELECT", redis_db); err != nil {
 					return nil, err
 				}
 			}
