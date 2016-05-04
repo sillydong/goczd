1f3211bfd07d9dd1568a5e72a18643c6b4135037
Chen.Zhidong <njutczd@gmail.com>
2016-05-04 10:19:47 +0800

    引入hprose rpc初始化

---
+++ gorpc/gorpc.go
@@ -0,0 +1,67 @@
+package gorpc
+
+import (
+	"github.com/hprose/hprose-go"
+	"github.com/sillydong/goczd/golog"
+	"net/http"
+	"reflect"
+)
+
+//response
+type response struct {
+	Status bool
+	Data   interface{}
+	Error  string
+}
+
+func (r *response) run() {
+	return map[string]interface{}{
+		"status": r.Status,
+		"data":   r.Data,
+		"error":  r.Error,
+	}
+}
+
+func NewResponse(status bool, data interface{}, err error) map[string]interface{} {
+	return &response{Status: status, Data: data, Error: err.Error()}.run()
+}
+
+//event
+type event struct{}
+
+func (e *event) OnBeforeInvoke(name string, args []reflect.Value, byref bool, context hprose.Context) {
+}
+
+func (e *event) OnAfterInvoke(name string, args []reflect.Value, byref bool, result []reflect.Value, context hprose.Context) {
+}
+func (e *event) OnSendError(err error, context hprose.Context) {
+	golog.Error(err, context)
+}
+
+//init rpc
+func InitRpc(listentcp, listenhttp, listensocket string, debug bool, methods *hprose.Methods) {
+	if listensocket != "" {
+		golog.Info("listening on unix:" + listensocket)
+		service := hprose.NewUnixServer("unix:" + listensocket)
+		service.DebugEnabled = debug
+		service.ServiceEvent = &event{}
+		service.Methods = methods
+		service.Start()
+	} else if listentcp != "" {
+		golog.Info("listening on tcp://" + listentcp)
+		service := hprose.NewTcpServer("tcp://" + listentcp)
+		service.DebugEnabled = debug
+		service.ServiceEvent = &event{}
+		service.Methods = methods
+		service.Start()
+	} else if listenhttp != "" {
+		golog.Info("listening on http://" + listenhttp)
+		service := hprose.NewHttpService()
+		service.DebugEnabled = debug
+		service.ServiceEvent = &event{}
+		service.Methods = methods
+		http.ListenAndServe(listenhttp, service)
+	} else {
+		panic("missing listen configuration")
+	}
+}
