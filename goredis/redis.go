54b307f58b56981fecbe8d3489b941332f8bb314
Chen.Zhidong <njutczd@gmail.com>
2016-05-06 23:41:55 +0800

    redis初始化支持redigo和goredis

--- goredis/redis.go
+++ goredis/redis.go
@@ -1,11 +1,12 @@
 package goredis
 
 import (
-	"github.com/garyburd/redigo/redis"
+	redigo "github.com/garyburd/redigo/redis"
+	goredis "gopkg.in/redis.v3"
 	"time"
 )
 
-func InitRedis(host, port, socket string, db int, password string) (client *redis.Pool) {
+func InitRedigo(host, port, socket string, db int, password string) (client *redigo.Pool) {
 	var network, addr string
 	if socket != "" {
 		network = "unix"
@@ -17,9 +18,9 @@ func InitRedis(host, port, socket string, db int, password string) (client *redi
 		panic("redis configuration error")
 	}
 
-	client = &redis.Pool{
-		Dial: func() (redis.Conn, error) {
-			conn, err := redis.Dial(network, addr, redis.DialDatabase(db), redis.DialPassword(password))
+	client = &redigo.Pool{
+		Dial: func() (redigo.Conn, error) {
+			conn, err := redigo.Dial(network, addr, redigo.DialDatabase(db), redigo.DialPassword(password))
 			if err != nil {
 				return nil, err
 			} else {
@@ -30,4 +31,32 @@ func InitRedis(host, port, socket string, db int, password string) (client *redi
 	}
 
 	return client
+}
+
+func InitRedisv3(host, port, socket string, db int64, password string) (client *goredis.Client, err error) {
+	if socket != "" {
+		client = goredis.NewClient(&goredis.Options{
+			Network:     "unix",
+			Addr:        socket,
+			Password:    password,
+			DB:          db,
+			MaxRetries:  2,
+			IdleTimeout: 60 * time.Second,
+		})
+	} else if host != "" && port != "" {
+		client = goredis.NewClient(&goredis.Options{
+			Network:     "tcp",
+			Addr:        host + ":" + port,
+			Password:    password,
+			DB:          db,
+			MaxRetries:  2,
+			IdleTimeout: 60 * time.Second,
+		})
+	} else {
+		panic("redis configuration error")
+	}
+
+	_, err = client.Ping().Result()
+
+	return
 }
