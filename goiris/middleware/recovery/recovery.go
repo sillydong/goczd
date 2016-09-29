ad85b081b946dcc68b06575742353d586a441a91
Chen.Zhidong <njutczd@gmail.com>
2016-09-30 00:16:58 +0800

    清除多余时间

--- goiris/middleware/recovery/recovery.go
+++ goiris/middleware/recovery/recovery.go
@@ -2,7 +2,6 @@ package recovery
 
 import (
 	"runtime/debug"
-	"time"
 
 	"github.com/kataras/iris"
 	"github.com/sillydong/goczd/golog"
@@ -14,7 +13,7 @@ type Recovery struct {
 func (r *Recovery) Serve(ctx *iris.Context) {
 	defer func() {
 		if err := recover(); err != nil {
-			golog.Criticalf(time.Now().Format("Mon, 02 Jan 2006 15:04:05 GMT")+": Recovery from panic\n%s\n%s\n", err, debug.Stack())
+			golog.Criticalf("Recovery from panic\n%s\n%s\n", err, debug.Stack())
 			//ctx.Panic just sends  http status 500 by default, but you can change it by: iris.OnPanic(func( c *iris.Context){})
 			ctx.Panic()
 		}
