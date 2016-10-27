491cbc79e9f919573a7d832dc7afa402492a392a
Chen.Zhidong <njutczd@gmail.com>
2016-10-27 09:44:06 +0800

    计算月份

--- gotime/time.go
+++ gotime/time.go
@@ -2,6 +2,7 @@ package gotime
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -61,4 +62,41 @@ func FriendlyTime(timestamp int64) string {
 	default:
 		return TimeToStr(timestamp, Y+"-"+M+"-"+D)
 	}
+}
+
+//显示指定月份前后一个月份
+func CalcMonth(yearmonth string) (string, string, string, error) {
+	if len(yearmonth) != 6 {
+		return "", "", "", fmt.Errorf("%v", "yearmonth format should be 201601")
+	}
+	year := yearmonth[0:4]
+	month := yearmonth[4:6]
+	iyear, _ := strconv.Atoi(year)
+	switch month {
+	case "01":
+		return strconv.Itoa(iyear-1) + "12", year + "01", year + "02", nil
+	case "02":
+		return year + "01", year + "02", year + "03", nil
+	case "03":
+		return year + "02", year + "03", year + "04", nil
+	case "04":
+		return year + "03", year + "04", year + "05", nil
+	case "05":
+		return year + "04", year + "05", year + "06", nil
+	case "06":
+		return year + "05", year + "06", year + "07", nil
+	case "07":
+		return year + "06", year + "07", year + "08", nil
+	case "08":
+		return year + "07", year + "08", year + "09", nil
+	case "09":
+		return year + "08", year + "09", year + "10", nil
+	case "10":
+		return year + "09", year + "10", year + "11", nil
+	case "11":
+		return year + "10", year + "11", year + "12", nil
+	case "12":
+		return year + "11", year + "12", strconv.Itoa(iyear+1) + "01", nil
+	}
+	return "", "", "", fmt.Errorf("%v", "yearmonth format should be 201601")
 }
